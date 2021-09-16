f88b6990c0d10f1019a7c66f8e9b910b2d797f46
itissandeep98 <itissandeep98@gmail.com>
2021-09-16 20:04:00 +0530

    packages update

--- app/src/App.js
+++ app/src/App.js
@@ -2,29 +2,27 @@ import React from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
 import { Drizzle } from "@drizzle/store";
 import drizzleOptions from "./drizzleOptions";
-import MyComponent from "./MyComponent";
+import MyComponent from "./Components/MyComponent";
 import "./App.css";
 
 const drizzle = new Drizzle(drizzleOptions);
 
 const App = () => {
-  return (
-    <DrizzleContext.Provider drizzle={drizzle}>
-      <DrizzleContext.Consumer>
-        {drizzleContext => {
-          const { drizzle, drizzleState, initialized } = drizzleContext;
+	return (
+		<DrizzleContext.Provider drizzle={drizzle}>
+			<DrizzleContext.Consumer>
+				{(drizzleContext) => {
+					const { drizzle, drizzleState, initialized } = drizzleContext;
 
-          if (!initialized) {
-            return "Loading..."
-          }
+					if (!initialized) {
+						return "Loading...";
+					}
 
-          return (
-            <MyComponent drizzle={drizzle} drizzleState={drizzleState} />
-          )
-        }}
-      </DrizzleContext.Consumer>
-    </DrizzleContext.Provider>
-  );
-}
+					return <MyComponent drizzle={drizzle} drizzleState={drizzleState} />;
+				}}
+			</DrizzleContext.Consumer>
+		</DrizzleContext.Provider>
+	);
+};
 
 export default App;
--- app/src/Components/MyComponent.js
+++ app/src/Components/MyComponent.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { newContextComponents } from "@drizzle/react-components";
-import logo from "./logo.png";
 
 const { AccountData, ContractData, ContractForm } = newContextComponents;
 
-export default ({ drizzle, drizzleState }) => {
+const MyComponent = ({ drizzle, drizzleState }) => {
 	// destructure drizzle and drizzleState from props
 	return (
 		<div className="App">
-			<div>
-				<img src={logo} alt="drizzle-logo" />
-				<h1>Drizzle Examples</h1>
-				<p>
-					Examples of how to get started with Drizzle in various situations.
-				</p>
-			</div>
-
 			<div className="section">
 				<h2>Active Account</h2>
 				<AccountData
@@ -127,3 +118,5 @@ export default ({ drizzle, drizzleState }) => {
 		</div>
 	);
 };
+
+export default MyComponent;
